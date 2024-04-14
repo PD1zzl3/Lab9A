47d8e24bb1334df49a4fa80c1f6e6934c6b10ae6
Peyton <PEytonlefleur@icloud.com>
2024-04-13 20:17:11 -0400

    Encoder

--- Lab9A.py
+++ Lab9A.py
@@ -5,3 +5,17 @@ def encoder(password):
         encodedPassword += digit
     return encodedPassword
 
+def main():
+    print('Menu' +
+    '\n -------------')
+
+    print(' 1. Encode' +
+          '\n 2. Decode' +
+          '\n 3.Quit')
+
+    while True:
+        print('Please enter an option: ')
+        choice = int(input(''))
+        if choice == 1:
+            password = input('Please enter your passweord to encode: ')
+            encodedPassword = encoder(password)
